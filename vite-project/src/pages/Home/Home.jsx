279cff4de3397c80cf3ae930c4c05b712a8cb730
Lishguy <lishguyy@gmail.com>
2025-02-02 13:38:45 +0100

    Adjusted my home web page

--- vite-project/src/pages/Home/Home.jsx
+++ vite-project/src/pages/Home/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
       <div className="home">
         <div className="hero">
             <h1>Largest <br /> Crypto Marketplace</h1>
-            <p>Welcome to the world's largest cryptocurrency
+            <p>Welcome to the World largest cryptocurrency
                 marketplace. Sign up to explore more about crypto.
             </p>
             <form onSubmit={searchHandler}>
